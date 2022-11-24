8bf2ef85b0d3e4da757f20806548bd6e3155a827
Christoph Schmatzler <christoph@schmatzler.dev>
2022-11-24 22:11:45 +0100

    WIP

--- config/runtime.exs
+++ config/runtime.exs
@@ -68,7 +68,6 @@ if config_env() == :prod do
   config :leuchtturm, Leuchtturm.Web.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
