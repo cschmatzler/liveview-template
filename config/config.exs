b4a1120cd8555c932db971a620e58620a9c1e547
Christoph Schmatzler <christoph@medium.place>
2023-03-24 20:03:57 +0100

    chore(deps): bump build tools

--- config/config.exs
+++ config/config.exs
@@ -23,16 +23,16 @@ config :leuchtturm,
 # Web Assets
 # ------
 config :esbuild,
-  version: "0.15.15",
+  version: "0.17.13",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2020 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
 config :tailwind,
-  version: "3.2.4",
+  version: "3.2.7",
   default: [
     args: ~w(
       --config=tailwind.config.js
