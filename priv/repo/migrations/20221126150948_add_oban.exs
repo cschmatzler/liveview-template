31a7555d18412b29f86545af7762259e5e46bf17
Christoph Schmatzler <christoph@medium.place>
2023-03-27 20:38:21 +0200

    Fix Oban migrations

--- priv/repo/migrations/20221126150948_add_oban.exs
+++ priv/repo/migrations/20221126150948_add_oban.exs
@@ -1,13 +1,11 @@
 defmodule Leuchtturm.Repo.Migrations.AddOban do
   use Ecto.Migration
 
-  use Ecto.Migration
-
   def up do
     Oban.Migrations.up(version: 11, prefix: "jobs")
   end
 
   def down do
-    Oban.Migrations.down(version: 1, prefix: "jobs")
+    Oban.Migrations.down(version: 11, prefix: "jobs")
   end
 end
