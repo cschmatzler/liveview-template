ed78c266cd2e29899652ccdc8d612e3629099e85
Christoph Schmatzler <christoph@medium.place>
2023-03-28 20:28:56 +0200

    WIP: auth

---
+++ lib/leuchtturm/auth/user.ex
@@ -0,0 +1,36 @@
+defmodule Leuchtturm.Auth.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+  import Ecto.Query
+
+  @schema_prefix "auth"
+  @timestamps_opts [type: :utc_datetime]
+  schema "users" do
+    field :provider, :string
+    field :uid, :string
+    field :name, :string
+    field :image_url, :string
+    timestamps()
+  end
+
+  @type t :: %__MODULE__{
+          id: integer(),
+          provider: String.t(),
+          uid: String.t(),
+          name: String.t(),
+          image_url: String.t() | nil
+        }
+
+  def changeset(%__MODULE__{} = user \\ %__MODULE__{}, attrs) do
+    user
+    |> cast(attrs, [:provider, :uid, :name, :image_url])
+    |> validate_required([:provider, :uid, :name])
+  end
+
+  def by_oauth_query(provider, uid) do
+    from u in __MODULE__,
+      where: u.provider == ^provider,
+      where: u.uid == ^uid
+  end
+end
