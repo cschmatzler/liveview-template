216ee9deea6449ab65282e60400c5811b50759e4
Christoph Schmatzler <christoph@medium.place>
2023-03-29 15:23:48 +0200

    fix configcat in prod

--- config/prod.exs
+++ config/prod.exs
@@ -8,10 +8,11 @@ config :logger, level: :info
 # -------------
 # Feature Flags
 # -------------
-config :template, ConfigCat, enabled?: true
+config :template, ConfigCat,
+  flag_overrides: %{},
+  flag_override_strategy: :remote_over_local
 
 # ---
 # Web
 # ---
-config :template, Template.Web.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+config :template, Template.Web.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
