d4defc4f2b80e1b3684632763deaca987e2dc52d
Christoph Schmatzler <christoph@medium.place>
2023-03-30 19:14:31 +0200

    test

--- lib/template/macro.ex
+++
@@ -1,8 +0,0 @@
-defmodule Auth.Macro do
-  defmacro __using__(options) do
-    quote bind_quoted: [options: options] do
-      Code.ensure_loaded!(Template.Auth.Behaviour)
-      |> IO.puts
-    end
-  end
-end
