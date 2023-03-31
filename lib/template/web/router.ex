7e1a4204e2fef3e3f0eefa8ffc42732bb08cb363
Christoph Schmatzler <christoph@medium.place>
2023-03-31 15:47:14 +0200

    fmt

--- lib/template/web/router.ex
+++ lib/template/web/router.ex
@@ -14,7 +14,11 @@ defmodule Template.Web.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {Template.Web.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self' 'unsafe-eval' 'unsafe-inline'"}
+
+    plug :put_secure_browser_headers, %{
+      "content-security-policy" => "default-src 'self' 'unsafe-eval' 'unsafe-inline'"
+    }
+
     plug :fetch_user
   end
 
