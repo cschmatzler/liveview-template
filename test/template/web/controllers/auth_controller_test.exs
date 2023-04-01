e7d42538c7766a1d0acd6ce01f32052046c388cf
Christoph Schmatzler <christoph@medium.place>
2023-04-01 21:06:35 +0200

    remove tests that need oauth credentials

--- test/template/web/controllers/auth_controller_test.exs
+++ test/template/web/controllers/auth_controller_test.exs
@@ -4,18 +4,6 @@ defmodule Template.Web.Controllers.AuthControllerTest do
   alias Template.Web.Auth
 
   describe "request" do
-    test "redirects to Google when provider is Google", %{conn: conn} do
-      conn = get(conn, "/auth/google")
-
-      assert redirected_to(conn) =~ "google.com"
-    end
-
-    test "redirects to GitHub when provider is GitHub", %{conn: conn} do
-      conn = get(conn, "/auth/github")
-
-      assert redirected_to(conn) =~ "github.com"
-    end
-
     test "redirects to signed out path and adds an error if provider is unknown", %{conn: conn} do
       conn = get(conn, "/auth/invalid_provider")
 
