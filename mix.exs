7375b8b6c7e1e8fd861b91d0eeaadb2a0d938f8d
Christoph Schmatzler <christoph@medium.place>
2023-04-02 17:01:32 +0200

    update aliases

--- mix.exs
+++ mix.exs
@@ -106,16 +106,16 @@ defmodule Template.MixProject do
 
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "excoveralls.html"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": [
         "cmd npm i --prefix ./assets",
         "tailwind default --minify",
         "esbuild default --minify",
         "phx.digest"
       ],
+      "assets.reset": ["cmd rm -rf priv/static/assets"],
       deploy: ["deps.get", "assets.deploy", "release"]
     ]
   end
