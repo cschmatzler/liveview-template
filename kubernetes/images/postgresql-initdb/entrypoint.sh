b8e778e9785dc0d92b9eebec6987ab397aae0946
Christoph Schmatzler <christoph@medium.place>
2023-04-21 15:33:23 +0200

    wip

---
+++ kubernetes/images/postgresql-initdb/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+export PGHOST="${POSTGRES_HOST}"
+export PGUSER="${POSTGRES_SUPER_USER}"
+export PGPASSWORD="${POSTGRES_SUPER_PASS}"
+
+if [[ -z "${PGHOST}" || -z "${PGUSER}" || -z "${PGPASSWORD}" || -z "${POSTGRES_USER}" || -z "${POSTGRES_PASS}" || -z "${POSTGRES_DB}" ]]; then
+  echo "Invalid configuration..."
+  exit 1
+fi
+
+until pg_isready; do
+  sleep 1
+done
+
+user_exists=$(\
+  psql \
+    --tuples-only \
+    --csv \
+    --command "SELECT 1 FROM pg_roles WHERE rolname = '${POSTGRES_USER}'"
+  )
+
+if [[ -z "${user_exists}" ]]; then
+  createuser ${POSTGRES_USER_FLAGS} "${POSTGRES_USER}"
+  psql --command "alter user \"${POSTGRES_USER}\" with encrypted password '${POSTGRES_PASS}';"
+fi
+
+database_exists=$(\
+  psql \
+    --tuples-only \
+    --csv \
+    --command "SELECT 1 FROM pg_database WHERE datname = '${POSTGRES_DB}'"
+  )
+
+if [[ -z "${database_exists}" ]]; then
+  createdb --owner "${POSTGRES_USER}" "${POSTGRES_DB}"
+fi
+
+psql --command "grant all privileges on database \"${POSTGRES_DB}\" to \"${POSTGRES_USER}\";"
+
