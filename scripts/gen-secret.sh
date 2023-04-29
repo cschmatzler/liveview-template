00c93419731d075fe5c122fc63f9a27fefe28ad0
Christoph Schmatzler <christoph@medium.place>
2023-04-30 00:10:11 +0200

    wip

--- scripts/gen-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -eo pipefail
-set -a
-
-source $1.secret.env
-sops --age $AGE_PUBLIC_KEY --input-type dotenv -e $1.secret.env > $1.env.enc
-gomplate -f $1.tpl | sops --age $AGE_PUBLIC_KEY --input-type yaml --encrypted-regex '^(data|stringData)$' -e /dev/stdin > $1.enc
