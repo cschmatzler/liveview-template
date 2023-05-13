3d53256a911c8a6b8d715f65be94803b81cd0610
Christoph Schmatzler <christoph@medium.place>
2023-05-13 12:33:13 +0200

    wip
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "hcloud_token" {
   type      = string
   sensitive = true
+  default = "${env("HCLOUD_TOKEN")}"
 }
 
 variable "cloudflare_token" {
   type      = string
   sensitive = true
+  default = "${env("CLOUDFLARE_TOKEN")}"
 }
