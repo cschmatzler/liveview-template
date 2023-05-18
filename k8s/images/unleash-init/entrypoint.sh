a8795f713c743389ba996908a55d6c7e87a7f5ed
Christoph Schmatzler <christoph@medium.place>
2023-05-18 13:08:48 +0200

    wip
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

---
+++ k8s/images/unleash-init/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ -z "${UNLEASH_URL}" || -z "${API_TOKEN}" || -z "${USERS}" ]]; then
+  echo "Invalid configuration..."
+  exit 1
+fi
+
+until $(curl --output /dev/null --silent --head --fail $UNLEASH_URL); do
+    printf 'Waiting for Unleash to become available...'
+    sleep 5
+done
+
+IFS=', ' read -r -a users <<< $USERS
+for user in "${users[@]}"
+do
+  IFS=':' read -r -a user_data <<< $user
+  curl \
+    "$UNLEASH_URL/api/admin/user-admin" \
+    --header "Authorization: Bearer $API_TOKEN" \
+    --header "Content-Type: application/json" \
+    --data '{"email":"'$user_data[0]'","username":"'$user_data[1]'","rootRole":"Admin","sendEmail":false}'
+done
